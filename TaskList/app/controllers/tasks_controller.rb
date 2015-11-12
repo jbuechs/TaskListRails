35b605d632be83c6eb68b85933cfd6fc6cc21146
Jennie Buechner <janina.buechner@gmail.com>
2015-11-12 12:10:35 -0800

    Format new form

--- TaskList/app/controllers/tasks_controller.rb
+++ TaskList/app/controllers/tasks_controller.rb
@@ -9,4 +9,21 @@ class TasksController < ApplicationController
     id = params[:id]
     @tasks = Task.where(id: id)
   end
+
+  def new
+    @title = "Add a Task"
+    @task = Task.new()
+  end
+
+  # def create
+  #   # save the new task using the task_params
+  #   Task.create(task_params[:post])
+  #   redirect_to('/')
+  # end
+  #
+  # private
+  # def task_params
+  #   # params.permit(post:[:title, :publish_date, :author_name])
+  # end
+
 end
