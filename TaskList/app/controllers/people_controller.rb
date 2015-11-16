0f7b43612c57908ef91a0add5e2e3655addcc627
Jennie Buechner <janina.buechner@gmail.com>
2015-11-16 14:00:19 -0800

    create show view and action for each person

--- TaskList/app/controllers/people_controller.rb
+++ TaskList/app/controllers/people_controller.rb
@@ -1,6 +1,24 @@
 class PeopleController < ApplicationController
+  # before_action :get_person, only: [:show]
 
   def index
     @people = Person.all
   end
+
+  def show
+    id = params[:id]
+    @person = Person.find(id)
+    @people = [@person]
+  end
+
+  def new
+
+  end
+
+  # private
+  #   def get_person
+  #     id = params[:id]
+  #     @person = Person.find(id)
+  #   end
+
 end
